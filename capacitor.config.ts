1af24333c72ee4eb0aa2f307ab2433ee6a68d422
leocaseiro <leobok@gmail.com>
2022-11-10 20:50:06 +1100

    rename to dumidi-tutor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-sidemenu',
-  webDir: 'build',
-  bundledWebRuntime: false
+    appId: 'io.ionic.starter',
+    appName: 'drumidi-tutor',
+    webDir: 'build',
+    bundledWebRuntime: false,
 };
 
 export default config;
